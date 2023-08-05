2f8b57a05eb4993bed0f008441bfc5c8a7b50fd0
arnos-stuff <bcda0276@gmail.com>
2023-08-05 04:15:54 +0200

    added init script and details

---
+++ README.md
@@ -0,0 +1,20 @@
+# Scripts to kickstart a new server
+
+In order:
+
+```shell
+git clone --recurse-submodules https://github.com/arnos-stuff/wireguard-vps
+```
+Then just run
+
+```shell
+bash $HOME/git/wireguard-vps/install.sh
+```
+
+# Auto Install
+
+You can use the auto-install script from the releases:
+
+```shell
+bash -c "$(curl -sSL 'https://github.com/<path>')"
+```
--- install.sh
+++ install.sh
@@ -20,11 +20,11 @@ sudo apt-get install build-essential procps curl file git
 yes '' | ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 yes '' | sh -c "$(curl -sS https://starship.rs/install.sh)"
 up
-mkdir git
-git clone https://github.com/arnos-stuff/shell git/shell
-cp git/shell/prompts/half-pure.toml .config/starship.toml
-cp git/shell/zsh/zshrc.base.zsh .zshrc
-cp git/shell/zsh/zshenv.base.zsh .zshenv
-cp -r git/shell/apps/micro .config/
+mkdir -p $HOME/git
+git clone https://github.com/arnos-stuff/shell $HOME/git/shell
+cp $HOME/git/shell/prompts/half-pure.toml $HOME/.config/starship.toml
+cp $HOME/git/shell/zsh/zshrc.base.zsh $HOME/.zshrc
+cp $HOME/git/shell/zsh/zshenv.base.zsh $HOME/.zshenv
+cp -r $HOME/git/shell/apps/micro $HOME/.config/
 sudo chsh -s /bin/zsh
 exec zsh
